d875b89457255a0f3e89aff9a6221c2511a95835
Stephanie Shih <stephk.shih@gmail.com>
2020-06-03 15:54:58 -0700

    Added RecipeMedia and MaterialMedia db models.

--- model.py
+++ model.py
@@ -113,11 +113,11 @@ class RecipeMaterial(db.Model):
     __tablename__ = "recipematerials"
 
     # table columns
-    recipematerial_id = db.Column(db.Integer,
-                                primary_key=True,
-                                autoincrement=True,
-                                nullable=False,
-                                )
+    rcpmat_id = db.Column(db.Integer,
+                        primary_key=True,
+                        autoincrement=True,
+                        nullable=False,
+                        )
     recipe_id = db.Column(db.Integer,
                         db.ForeignKey('recipes.recipe_id'),
                         nullable=False,
@@ -135,7 +135,7 @@ class RecipeMaterial(db.Model):
     def __repr__(self):
         """Human-readable summary of a recipematerial."""
 
-        return (f'<RecipeMaterial recipematerial_id={self.recipematerial_id} '
+        return (f'<RecipeMaterial rcpmat_id={self.rcpmat_id} '
                 + f'recipe_id={self.recipe_id} '
                 + f'material_id={self.material_id} '
                 + f'qty={self.qty}>'
@@ -167,3 +167,88 @@ class Media(db.Model):
                 + f'file_path={self.file_path} '
                 + f'media_type={self.media_type}>'
                 )
+
+
+class RecipeMedia(db.Model):
+    """An individual media object for a recipe."""
+
+    # table name
+    __tablename__ = "recipemedias"
+
+    # table columns
+    rcpmed_id = db.Column(db.Integer,
+                        primary_key=True,
+                        autoincrement=True,
+                        nullable=False,
+                        )
+    recipe_id = db.Column(db.Integer,
+                        db.ForeignKey('recipes.recipe_id'),
+                        nullable=False,
+                        )
+    media_id = db.Column(db.Integer,
+                        db.ForeignKey('medias.media_id'),
+                        nullable=False,
+                        )
+
+    # relationships
+    # TODO: fill in
+
+    def __repr__(self):
+        """Human-readable summary of a recipemedia object."""
+
+        return (f'<RecipeMedia rcpmed_id={self.rcpmed_id} '
+                + f'recipe_id={self.recipe_id} '
+                + f'media_id={self.media_id}>')
+
+
+class MaterialMedia(db.Model):
+    """An individual media object for a material."""
+
+    # table name
+    __tablename__ = "materialmedias"
+
+    # table columns
+    matmed_id = db.Column(db.Integer,
+                        primary_key=True,
+                        autoincrement=True,
+                        nullable=False,
+                        )
+    material_id = db.Column(db.Integer,
+                            db.ForeignKey('materials.material_id'),
+                            nullable=False,
+                            )
+    media_id = db.Column(db.Integer,
+                        db.ForeignKey('medias.media_id'),
+                        nullable=False,
+                        )
+
+    # relationships
+    # TODO: fill in
+
+    def __repr__(self):
+        """Human-readable summary of a materialmedia object."""
+
+        return (f'<MaterialMedia matmed_id={self.matmed_id} '
+                + f'material_id={self.material_id} '
+                + f'media_id={self.media_id}>')
+
+
+def connect_to_db(flask_app, db_uri='postgresql:///nookcookbook', echo=True):
+    """Connect to database."""
+
+    # Set flask app configurations
+    flask_app.config['SQlALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+
+# if __name__ == '__main__':
+    # from server import app
+
+    # connect_to_db(app)
+    # connect_to_db(app, echo=False) # if want turn off SQLAlchemy query echoing
