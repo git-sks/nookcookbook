95a3c3089e0a57c29a354262f585c2c8aff830f8
Stephanie Shih <stephk.shih@gmail.com>
2020-07-02 08:53:11 -0700

    Fixed materials calculation

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -57,6 +57,10 @@ class App extends React.Component {
     // go through by key each rcp in the calcRcps dict prop passed from App
     // get the rcp information from the server
     // then get the list of materials from the server response
+
+    // instantiate a new calcMats list to track materials
+    const newCalcMats = [];
+
     for (const rcpID in this.state.calcRcps) {
       fetch(`/api/recipes/${rcpID}`)
         .then(response => response.json())
@@ -65,8 +69,6 @@ class App extends React.Component {
 
           // for each material dictionary in the list of materials
           for (const mat of data['materials']) {
-            // make a copy of the current calcMats state
-            const newCalcMats = this.state.calcMats;
 
             //if the material isn't in the dictionary yet
             if (newCalcMats[mat['id']] === undefined) {
@@ -78,7 +80,7 @@ class App extends React.Component {
               // if the material is in the dictionary, update the qty in the
               // dictionary by the qty of material per recipe * qty of the
               // recipe in the recipe calculator
-              newCalcMats[mat['id']]['qty'] = mat['qty'] * rcp['qty'];
+              newCalcMats[mat['id']]['qty'] = newCalcMats[mat['id']]['qty'] + mat['qty'] * rcp['qty'];
             }
 
             // set the state of the current calcMats to the updated newCalcMats
